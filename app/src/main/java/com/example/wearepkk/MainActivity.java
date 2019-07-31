1dcdc1f7fec0f5c21e0a220a691dc604ec72102f
manajemen-keuangan <46679882+manajemen-keuangan@users.noreply.github.com>
2019-07-31 20:17:43 +0700

    Validasi Koneksi selesai

--- app/src/main/java/com/example/wearepkk/MainActivity.java
+++ app/src/main/java/com/example/wearepkk/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.wearepkk;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 import androidx.customview.widget.ViewDragHelper;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.util.Log;
+import android.net.*;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -25,6 +31,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
         eventcard = findViewById(R.id.id_event_card);
@@ -34,6 +41,41 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         eventcard.setOnClickListener(this);
         pediacard.setOnClickListener(this);
         aboutcard.setOnClickListener(this);
+        if (!isConnected(MainActivity.this))
+            buildDialog(MainActivity.this).show();
+        else {
+
+        }
+
+    }
+    public boolean isConnected(Context context){
+
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netinfo = cm.getActiveNetworkInfo();
+
+        if (netinfo != null && netinfo.isConnectedOrConnecting()) {
+            android.net.NetworkInfo wifi = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+            android.net.NetworkInfo mobile = cm.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+
+            if ((mobile != null && mobile.isConnectedOrConnecting()) || (wifi != null & wifi.isConnectedOrConnecting()))
+                return true;
+            else
+                return false;
+        }else
+            return false;
+
+    }
+    public AlertDialog.Builder buildDialog(Context c){
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(c);
+        builder.setTitle("No Internet Connection");
+        builder.setMessage("Tolong nyalakan wifi anda atau data mobile anda, untuk meload data aplikasi");
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+            }
+        });
+        return builder;
 
     }
 
